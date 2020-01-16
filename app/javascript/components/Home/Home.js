e8ac361b5523fc4f6d43bd97b3607c618e779a37
Bailey <bailey.g.weathers@gmail.com>
2020-01-15 21:20:35 -0500

    Complete bootstrap setup and home & jumbotron components

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Jumbotron from './Jumbotron'
 
 class Home extends Component {
   constructor() {
@@ -6,17 +7,19 @@ class Home extends Component {
 
     this.state = {
       course_modules: [
-        { id: 1, title: '', description: 'loreum ipsum', active: false },
-        { id: 2, title: '', description: '', active: false },
-        { id: 3, title: '', description: '', active: false },
-        { id: 4, title: '', description: '', active: false },
+        { id: 1, title: '1. test 1', description: 'loreum ipsum', active: false },
+        { id: 2, title: '2. test 2', description: 'loreum ipsum', active: false },
+        { id: 3, title: '3. test 3', description: 'loreum ipsum', active: false },
+        { id: 4, title: '4. test 4', description: 'loreum ipsum', active: false },
       ]
     }
   }
 
   render() {
     return (
-      <div>This is our Home component.</div>
+      <div>
+        <Jumbotron/>
+      </div>  
     )
   }
 }
