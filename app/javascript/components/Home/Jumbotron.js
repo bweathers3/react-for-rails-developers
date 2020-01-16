894e54681f24e70fb028a01e69345c74d7c48b62
Bailey <bailey.g.weathers@gmail.com>
2020-01-16 16:14:19 -0500

    Complete basic styled-components

--- app/javascript/components/Home/Jumbotron.js
+++ app/javascript/components/Home/Jumbotron.js
@@ -1,14 +1,35 @@
 import React, { Component } from 'react'
+import styled from 'styled-components'
+
+const Section = styled.section`
+  background-color: #d74234;
+  min-height: 550px;
+  padding: 100px 0;
+  color: #fff;
+`
+
+const Header = styled.h1 `
+  color: #fff;
+  font-weight: 700;
+  font-size: 40px;
+  line-height: 52px;
+`
+
+const Subhead = styled.p `
+  color: #fff;
+  font-weight: 500;
+  font-size: 18px;
+`
 
 const Jumbotron = () => {
   return (
-    <section className="home-section-1">
+    <Section className="home-section-1">
       <div className="container">
         <div className="row">
           <div className="col col-sm-12 col-md-5">
             <div className="pt-4 mt-4">
-              <h1>React for Rails Developers</h1>
-              <p>Supercharge your Ruby on Rails Apps with React.js</p>
+              <Header>React for Rails Developers</Header>
+              <Subhead>Supercharge your Ruby on Rails Apps with React.js</Subhead>
             </div>
           </div>
           <div className="col col-sm-12 col-md-7">
@@ -18,7 +39,7 @@ const Jumbotron = () => {
           </div>
         </div>
       </div>
-    </section>
+    </Section>
   )
 }
 
