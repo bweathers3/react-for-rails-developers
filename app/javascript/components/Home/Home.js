8d779a7153e99a3978311f93bc6c43264ef46866
Bailey <bailey.g.weathers@gmail.com>
2020-01-18 07:29:57 -0500

    Complete activeItem

--- app/javascript/components/Home/Home.js
+++ app/javascript/components/Home/Home.js
@@ -9,7 +9,7 @@ class Home extends Component {
     this.state = {
       course_modules: [
         { id: 1, title: '1. test 1', description: 'loreum ipsum', active: false },
-        { id: 2, title: '2. test 2', description: 'loreum ipsum', active: false },
+        { id: 2, title: '2. test 2', description: 'loreum ipsum', active: true },
         { id: 3, title: '3. test 3', description: 'loreum ipsum', active: false },
         { id: 4, title: '4. test 4', description: 'loreum ipsum', active: false }
       ]
